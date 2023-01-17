ccf2af835bf74408a528af16706646b1c3582ab1
Jessica Kincaid <4j.kincaid@gmail.com>
2023-01-17 15:17:19 -0600

    display artworks by individual participant on their profile

--- panelists/urls.py
+++ panelists/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.profiles, name="profiles"),
-    path('profile/<str:pk>', views.panelistProfile, name="panelist-profile"),
+
+    path('', views.profiles, name="profiles"),
+    path('profile/<str:pk>/', views.panelistProfile, name="panelist-profile"),
+    # path('account/', views.panelistAccount, name="account"),
 ]
