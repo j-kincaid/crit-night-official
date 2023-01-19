85107568f5e23905fe402cf89a808d1891bb4066
Jessica Kincaid <4j.kincaid@gmail.com>
2023-01-19 10:24:30 -0600

    Add temporary messages for incorrect password and login logout confirmation

--- panelists/views.py
+++ panelists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 
 from .models import Profile
@@ -19,7 +20,8 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist.')
+            messages.error(request, 'Username is does not exist.')
+
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
@@ -27,12 +29,13 @@ def loginUser(request):
         # logs user into browser's cookies
             return redirect('profiles')
         else:
-            print('Username or password is incorrect.')
+            messages.error(request, 'Username or password is incorrect.')
 
     return render(request, 'panelists/login_register.html')
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, 'You have been logged out.')
     return redirect('login')
 
 
