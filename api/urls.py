86a5477b41d5b30830fab825b31726102b6f1ba4
Jessica Kincaid <4j.kincaid@gmail.com>
2023-03-06 12:13:14 -0600

    delete api directory

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoutes),
-    path('artworks/', views.getArtworks),
-    path('artworks/<str:pk>/', views.getArtwork),
-]
